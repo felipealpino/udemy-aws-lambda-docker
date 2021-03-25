326e8b11aed38054a1c572118e1ac6885cc9712b
Felipe Alpino <felipealpino1@gmail.com>
2021-03-25 19:01:27 -0300

    tratando event.httpMethod

---
+++ hello-world/app.js
@@ -0,0 +1,46 @@
+// const axios = require('axios')
+// const url = 'http://checkip.amazonaws.com/';
+let response;
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+exports.lambdaHandler = async (event, context) => {
+    try {
+        if(event.httpMethod == "GET"){
+            // const ret = await axios(url);
+            response = {
+                'statusCode': 200,
+                'body': JSON.stringify({
+                    message:"opa2",
+                    // location: ret.data.trim()
+                })
+            }
+        } else if(event.httpMethod == "POST"){
+            response = {
+                'statusCode': 200,
+                'body': JSON.stringify({
+                    message:{
+                        nome:"Felipe",
+                        idade:26
+                    },
+                    // location: ret.data.trim()
+                })
+            }
+        }   
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
